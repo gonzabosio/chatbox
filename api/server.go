8ceca012d530982858044e72c409d8acd918bb76
Gonzalo Bosio <gonzaabosio@gmail.com>
2024-08-17 16:12:15 -0300

    MongoDB connection

--- api/server.go
+++ api/server.go
@@ -1,20 +1,62 @@
 package api
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type App struct {
 	router *chi.Mux
+	client *mongo.Client
 }
 
-func (a *App) InitServer() {
+func (a *App) InitServer() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+	// New client and server connection
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(os.Getenv("ATLAS_URI")).SetServerAPIOptions(serverAPI)
+	ctx, cancelCtx := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelCtx()
+	a.client, err = mongo.Connect(ctx, opts)
+	if ctx.Err() == context.DeadlineExceeded {
+		return fmt.Errorf("timed out connecting MongoDB")
+	} else if ctx.Err() == context.Canceled {
+		return fmt.Errorf("connection to MongoDB canceled")
+	}
+	if err != nil {
+		return err
+	}
+	// Confirm successful connection sending a ping
+	if err := a.client.Database("chat_box").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+		return err
+	}
+	fmt.Println("Successfully connected to MongoDB!")
+
 	a.router = chi.NewRouter()
 	a.router.Post("/login", login)
+	return nil
 }
 
 func (a *App) Run() error {
 	return http.ListenAndServe(":8000", a.router)
+}
+
+func (a *App) ShutdownConn() {
+	if err := a.client.Disconnect(context.TODO()); err != nil {
+		fmt.Printf("Mongo disconnection failed: %v", err)
+	} else {
+		fmt.Println("MongoDB Disconnected")
+	}
 }
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/gonzabosio/chat-box/api"
 )
 
 func main() {
 	app := &api.App{}
-	app.InitServer()
-
-	if err := app.Run(); err != nil {
-		log.Fatalf("Server run failed")
+	if err := app.InitServer(); err != nil {
+		log.Fatalf("Could not start server instance: %v", err)
 	}
+
+	go func() {
+		if err := app.Run(); err != nil {
+			log.Fatalf("Server run failed: %v", err)
+		}
+	}()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh)
+	<-signalCh
+	app.ShutdownConn()
 }
